4ad1b3067b8a0a31825b6aad601a0fe5a2026bc8
viethung <viethungdev23@gmail.com>
2024-01-13 00:18:39 +0700

    add logs

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -9,18 +9,22 @@ using Net.payOS.Types;
 public class PaymentController : ControllerBase
 {
     private readonly PayOS _payOS;
+    private readonly ILogger _logger;
 
-    public PaymentController(PayOS payOS)
+    public PaymentController(PayOS payOS,ILogger logger)
     {
         _payOS = payOS;
+        _logger = logger;
     }
 
     [HttpPost("payos_transfer_handler")]
     public IActionResult payOSTransferHandler(WebhookType body)
     {
+        _logger.LogInformation("Đã vào hàm ròi -- 1");
         try
         {
             WebhookData data = _payOS.verifyPaymentWebhookData(body);
+            _logger.LogInformation($"đax gọi hàm VerifyData {data.amount}, {data.orderCode},{data.orderCode}");
 
             if (data.description == "Ma giao dich thu nghiem" || data.description == "VQRIO123")
             {
